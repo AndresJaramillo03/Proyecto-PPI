d1c8c423b05cdcdf7c55af769e3f1d48aa96ca3d
andresjaramillo03 <andresfjaramillo7233@gmail.com>
2025-05-06 23:23:44 -0500

    Cambio en la interfaz

--- Proyecto GST/frontend/src/pages/Home.jsx
+++ Proyecto GST/frontend/src/pages/Home.jsx
@@ -19,7 +19,38 @@ export default function Home() {
             </h4>
           </Card.Body>
         </Card>
+
       </Container>
+  
+      <Container className="d-flex justify-content-center align-items-center min-vh-100">
+        <Card className="custom-card shadow-lg">
+          <Card.Header className="custom-header text-white text-center">
+            <h3>Consejos para que tu dia sea mejor</h3>
+          </Card.Header>
+
+          <Card.Body className="text-center">
+            <h4 className="custom-text">Empieza con calma</h4>
+            <p className="custom-text">En lugar de saltar de la cama al estrés, dedica unos minutos a estirarte suavemente, 
+              respirar profundo o simplemente estar presente antes de revisar tu teléfono o empezar tus tareas.</p>
+          </Card.Body >
+
+          <Card.Body className="text-center">
+            <h4 className="custom-text">Muévete un poco</h4>
+            <p className="custom-text">La actividad física libera endorfinas, que tienen un efecto positivo en tu estado de ánimo. 
+              No tiene que ser un entrenamiento intenso; una caminata corta, bailar con tu canción favorita o hacer algunos estiramientos 
+              pueden marcar la diferencia.</p>
+          </Card.Body>
+
+          <Card.Body className="text-center">
+            <h4 className="custom-text">Conecta con otros</h4>
+            <p className="custom-text">Las interacciones sociales significativas son importantes para nuestra salud mental. 
+              Llama a un amigo, tómate un café con alguien querido o simplemente charla con un vecino.</p>
+          </Card.Body>
+
+        </Card>
+
+      </Container>
+
     </div>
   );
 }
